8cfad29e511ea29f2756e626b9b41a353f6c64e4
Sam Davies <sam.jc.davies@gmail.com>
2012-09-16 16:49:46 +0100

    Allow setting of timestamps

--- tempodb_proxy.rb
+++ tempodb_proxy.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'sinatra'
+require 'time'
 require 'tempodb'
 
 API_KEY = "cc0c654d01774b128c1e0495de51784b"
@@ -10,10 +11,13 @@ API_SECRET = "a280c43f6b27400998a4aba0b1eb4545"
 get '/data' do
   content_type :json
 
+  return [].to_json if (params[:start].nil? or Time.parse(params[:start]).nil?)
+  return [].to_json if (params[:stop].nil? or Time.parse(params[:stop]).nil?)
+
   client = TempoDB::Client.new(API_KEY, API_SECRET)
 
-  start = Time.utc(2012, 9, 15)
-  stop  = Time.utc(2012, 9, 16)
+  start = Time.parse params[:start]
+  stop  = Time.parse params[:stop]
   keys  = ["temperature"]
   data = client.read(start, stop, keys: keys, interval: "1min", function: "mean")
   data = data.first.data.map{ |dp| {ts: dp.ts, value: dp.value} }
