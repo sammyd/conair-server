c68431295af0dde18f17f2bdce17dcfb044f33bb
Sam Davies <sam.jc.davies@gmail.com>
2012-09-16 18:39:07 +0100

    Adds the front-end code and wires it up to the sinatra  app

---
+++ public/scripts/chart_plotter.js
@@ -0,0 +1,53 @@
+var context= cubism.context()
+    .serverDelay(2 * 60 * 1000) // Allow 2 mins server delay
+    .step(2 * 60 * 1000) // Every 2 mins
+    .size(940),
+    primary = temperature(),
+    secondary = primary.shift(- 24 * 60 * 60 * 1000);
+
+
+d3.select("#chart").call(function(div) {
+    div.append("div")
+      .attr("class", "axis")
+      .call(context.axis().orient("top"));
+    
+    div.selectAll(".horizon")
+      .data([primary])
+      .enter().append("div")
+      .attr("class", "horizon")
+      .call(context.horizon()
+        .height(120)
+        .format(d3.format(".2f"))
+        .title("Temperature"));
+
+    div.selectAll(".comparison")
+      .data([[primary, secondary]])
+      .enter().append("div")
+      .attr("class", "comparison")
+      .call(context.comparison()
+        .height(60)
+        .formatChange(d3.format(".1f%"))
+        .title("Daily Change"));
+
+    div.append("div")
+      .attr("class", "rule")
+      .call(context.rule());
+});
+
+context.on("focus", function(i) {
+    format = d3.format(".1f");
+    d3.selectAll(".horizon .value").style("right", i== null ? null : context.size() - i + "px")
+      .text(format(primary.valueAt(Math.floor(i))) + "\u00B0C");
+});
+
+
+function temperature() {
+    return context.metric(function(start, stop, step, callback) {
+        d3.json("/data/?start=" + start.toISOString()
+            + "&stop=" + stop.toISOString()
+            + "&step=" + step, function(data) {
+                if(!data) return callback(new Error("unable to load data"));
+                callback(null, data.map(function(d) { return d.value; }));
+            });
+    });
+}
--- tempodb_proxy.rb
+++ tempodb_proxy.rb
@@ -8,18 +8,28 @@ require 'tempodb'
 API_KEY = "cc0c654d01774b128c1e0495de51784b"
 API_SECRET = "a280c43f6b27400998a4aba0b1eb4545"
 
-get '/data' do
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/data/?' do
   content_type :json
 
   return [].to_json if (params[:start].nil? or Time.parse(params[:start]).nil?)
   return [].to_json if (params[:stop].nil? or Time.parse(params[:stop]).nil?)
+  return [].to_json if (params[:step].nil?)
 
   client = TempoDB::Client.new(API_KEY, API_SECRET)
 
   start = Time.parse params[:start]
   stop  = Time.parse params[:stop]
   keys  = ["temperature"]
-  data = client.read(start, stop, keys: keys, interval: "1min", function: "mean")
+  # Calculate the correct rollup
+  step = params[:step].to_i
+  # We'll just measure it in minutes
+  step = "#{step/60000}min"
+  logger.info step
+  data = client.read(start, stop, keys: keys, interval: step, function: "mean")
   data = data.first.data.map{ |dp| {ts: dp.ts, value: dp.value} }
   data.to_json
 end
