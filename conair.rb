b27b3aed93f8c44ce4b0c1852b4278d08ef11db4
Sam Davies <sam.jc.davies@gmail.com>
2012-09-17 22:16:40 +0100

    Updates the proxy to deal with discontinuous data
    We find missing points and adds a linear step between the points at
    either side.

--- conair.rb
+++ conair.rb
@@ -27,9 +27,29 @@ get '/data/?' do
   # Calculate the correct rollup
   step = params[:step].to_i
   # We'll just measure it in minutes
-  step = "#{step/60000}min"
+  step_string = "#{step/60000}min"
   logger.info step
-  data = client.read(start, stop, keys: keys, interval: step, function: "mean")
-  data = data.first.data.map{ |dp| {ts: dp.ts, value: dp.value} }
-  data.to_json
+  data = client.read(start, stop, keys: keys, interval: step_string, function: "mean")
+  #data = data.first.data.map{ |dp| {ts: dp.ts, value: dp.value} }
+  data = data.first.data
+  response_data = []
+  # We need to remove any inconsistencies
+  data.each_with_index do |val, index|
+    # Add this datapoint
+    response_data.push( { value: val.value } )
+    if (index + 1) < data.length
+      current_time = val.ts
+      next_time = data[index+1].ts
+      # If there is more than a 5 second difference
+      logger.info (next_time - current_time - step / 1000)
+      if((next_time - current_time - step / 1000).abs > 5)
+        # Let's add the right number of values
+        points_needed = ((next_time - current_time) / (step / 1000)).floor
+        difference = data[index+1].value - val.value
+        points_needed.times { |i| response_data.push({ value: (val.value + difference * i / points_needed.to_f) }) }
+      end
+    end
+  end
+
+  response_data.to_json
 end
